93f99efd88ad0d0fc5107df2c1c8e8ff120fdd02
dihjustino <40828980+dihjustino@users.noreply.github.com>
2018-07-04 09:53:48 -0300

    Update First.java

--- First/src/first/First.java
+++ First/src/first/First.java
@@ -21,6 +21,94 @@ public class First {
         System.out.printf("Programação Progressiva");
         System.out.printf("Programação Progressiva\n");
         System.out.printf("Programação Progressiva" + "");
+        /**
+         * 1. Frase na tela - Implemente um programa que escreve na tela a 
+         * frase "O primeiro programa agente nunca esquece!".
+         */
+        System.out.print("O primeiro programa a gente nunca esquece!\n");
+        /**
+         * 2. Etiqueta - Elabore um programa que escreve seu nome completo na  
+         * primeira linha, seu endereço na segunda, e o CEP e telefone na 
+         * terceira.
+         */
+        System.out.print("Sodré Jamon\n");
+        System.out.print("Rua Tadaski, 56\n");
+        System.out.print("Tel. +55 11 54987865 e CEP:08589-450\n");
+        /**
+         * Frases assassinas - Faça um programa que mostre na tela algumas 
+         * frases assassinas, que são aquelas que fazem com muitas idéias sejam 
+         * perdidas antes que amadureçam ou seja aprofundadas.
+         */
+        System.out.print("Isso nunca vai dar certo\n");
+        System.out.print("Você nunca será capaz de aprender isso!\n");
+        System.out.print("No dia que você aprender java o mundo acabará!\n");
+         /**
+         * 4. Mensagem - Escreva uma mensagem para uma pessoa de que goste. 
+         * Implemente um programa que imprima essa mensagem.
+         */          
+        System.out.print("Tudo no mundo é factível, até eu e você!\n");
+        /**
+         * 5. Ao mestre - Escreva um bilhete ao seu professor, informando seus 
+         * objetivos nesta disciplina e o que espera dela e do professor. 
+         * Implemente um programa que mostra seu bilhete na tela.
+         */
+        System.out.print("Aprender essa linguagem na mara!\n");
+        
+        /**
+         * Quadrado - Escrever um programa que mostre a seguinte figura no alto 
+         * da tela:
+         */
+        System.out.print("XXXXX\n"+"X   X\n"+"X   X\n"+"X   X\n"+"XXXXX\n");
+        /**
+         * 7. Tabela de notas - Escreva um programa que produza a seguinte 
+         * saída na tela:
+         */
+        System.out.print("ALUNO (A)           Nota\n");
+        System.out.print("==========          ====\n");
+        System.out.print("Aline               9.0\n");
+        System.out.print("Mário               DEZ\n");
+        System.out.print("Sérgio              4.5\n");
+        System.out.print("Shirley             7.0\n");
+        /**
+         * 8. Letra grande - Elabore um programa para produzir na tela a 
+         * letra J, de Java Progressivo, usando a própria. Se fosse ‘L’, 
+         * seria assim:
+         */
+        System.out.print("  JJJJJJJ\n");
+        System.out.print("      J \n");
+        System.out.print("       J \n");
+        System.out.print("  J    J \n");
+        System.out.print("   JJJJ \n");
+        /**
+         * 9. Menu - Elabore um programa que mostre o seguinte menu na tela:
+         * 0 - Fim
+         * 1 - Inclui
+         * 2 - Altera
+         * 3 - Exclui
+         * 4 - Consulta
+         */
+        System.out.print("Cadastro de Clientes\n");
+        System.out.print("0 - Fim\n");
+        System.out.print("1 - Inclui\n");
+        System.out.print("2 - Altera\n");
+        System.out.print("3 - Exclui\n");
+        System.out.print("4 - Consulta\n");
+        /**
+         * 10. Pinheiro  - Implemente um programa que desenhe um "pinheiro" 
+         * na tela, similar ao abaixo. Enriqueça o desenho com outros 
+         * caracteres, simulando enfeites.
+         */
+        System.out.print("        O\n");
+        System.out.print("       XXX\n");
+        System.out.print("      XX0XX\n");
+        System.out.print("     XXXXXXX\n");
+        System.out.print("    *XX*XX0XX\n");
+        System.out.print("   XX0XXXXXXXX\n");
+        System.out.print("  XXXXX$XXXX0XX\n");
+        System.out.print(" X%XX0XXX*XXXX%X\n");
+        System.out.print("  |     ||    |  \n");
+        System.out.print("  O     ||    O  \n");
+        System.out.print("       ====\n");
 // TODO code application logic here
     }
     
