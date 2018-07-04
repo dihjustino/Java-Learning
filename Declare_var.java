aca98a764abbafacc6107346ee474246c443a118
dihjustino <40828980+dihjustino@users.noreply.github.com>
2018-07-04 16:00:46 -0300

    Add files via upload

---
+++ Declare_var.java
@@ -0,0 +1,28 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package declare_var;
+
+/**
+ *
+ * @author brdjustin
+ */
+public class Declare_var {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        int idade=21;
+        System.out.print("Idade:");
+        System.out.print(idade+"\n");
+        System.out.print("Minha idade é " + idade + "\n");
+        
+        float reais=1.99f;
+        System.out.print("Só tenho R$" + reais + " na minha carateira");
+        // TODO code application logic here
+    }
+    
+}
