297e3b283e2d294f7e37004c89d4b9366b380d42
dihjustino <40828980+dihjustino@users.noreply.github.com>
2018-07-04 09:17:37 -0300

    Print lesson

---
+++ First/src/first/First.java
@@ -0,0 +1,27 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package first;
+
+/**
+ *
+ * @author brdjustin
+ */
+public class First {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        System.out.print("Meu primeiro programa em Java!\n");
+        System.out.print("Programação\n");
+        System.out.print("Progressiva\n");
+        System.out.printf("Programação Progressiva");
+        System.out.printf("Programação Progressiva\n");
+        System.out.printf("Programação Progressiva" + "");
+// TODO code application logic here
+    }
+    
+}
